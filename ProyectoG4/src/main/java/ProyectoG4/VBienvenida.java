eb6b47ec1d24514476f6458a04f795a766ea5f01
in2daw <in2daw@elorrieta.int>
2020-11-09 20:04:32 +0100

    corregir Bounds

--- ProyectoG4/src/main/java/ProyectoG4/VBienvenida.java
+++ ProyectoG4/src/main/java/ProyectoG4/VBienvenida.java
@@ -3,6 +3,7 @@ package ProyectoG4;
 	import java.util.Timer;
 	import java.util.TimerTask;
 	import javax.swing.*;
+import java.awt.Color;
 
 	public class VBienvenida extends JFrame {
 	    /**
@@ -15,46 +16,49 @@ package ProyectoG4;
 	   
 	    public VBienvenida(){
 	       
-	    	setLayout(null);
-	       
-	        bienvenido=new JLabel("¡Bienvenido/a!");
-	        bienvenido.setBounds(270,180,900,60);
-	        bienvenido.setFont(bienvenido.getFont().deriveFont(50f));
-	        add(bienvenido);
-	             
-	        bienvenido2=new JLabel("app diseñada para");
-	        bienvenido2.setBounds(305,300,900,60);
-	        bienvenido2.setFont(bienvenido2.getFont().deriveFont(30f));
-	        add(bienvenido2);
-	         
-	        cinecss=new JLabel("CINES CSS");
-	        cinecss.setBounds(350,350,900,60);
-	        cinecss.setFont(cinecss.getFont().deriveFont(30f));
-	        add(cinecss);
-	        
-	        text=new JLabel("A continuación podrás elegir la cartelera del próximo fin de semana");
-	        text.setBounds(390,500,900,60);
-	        text.setFont(text.getFont().deriveFont(15f));
-	        add(text);
-	       
 	        /*logo = new JLabel();
 	        ImageIcon Inter = new ImageIcon("../img/logo.png");
 	        logo.setIcon(Inter);
 	        logo.setBounds(250, 250, 500, 500);
 	        add(logo);*/
 
-	        
-	       Timer cronom=new Timer();
-	        cronom.schedule(new TimerTask(){
-	            
-	        public void run(){    
-	        	VLogin.tempologin=new VLogin();
-	        	VLogin.tempologin.setBounds(0,0,900,600);
-	        	VLogin.tempologin.setVisible(true);
-	        	VLogin.tempologin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	            dispose();
-	        }},3000);
-
+	    	initGUI();
+	    }
+	    private void initGUI() {
+	    	getContentPane().setBackground(new Color (156, 196, 236));
+	    	
+	    	getContentPane().setLayout(null);
+	    	
+	    	 bienvenido=new JLabel("¡Bienvenido/a!");
+	    	bienvenido.setBounds(52,67,345,60);
+	    	bienvenido.setFont(bienvenido.getFont().deriveFont(50f));
+	    	getContentPane().add(bienvenido);
+	    	
+	        bienvenido2=new JLabel("app diseñada para");
+	    	bienvenido2.setBounds(305,300,900,60);
+	    	bienvenido2.setFont(bienvenido2.getFont().deriveFont(30f));
+	    	getContentPane().add(bienvenido2);
+	    	
+	        cinecss=new JLabel("CINES CSS");
+	    	cinecss.setBounds(350,350,900,60);
+	    	cinecss.setFont(cinecss.getFont().deriveFont(30f));
+	    	getContentPane().add(cinecss);
+	    	
+	    	text=new JLabel("A continuación podrás elegir la cartelera del próximo fin de semana");
+	    	text.setBounds(390,500,900,60);
+	    	text.setFont(text.getFont().deriveFont(15f));
+	    	getContentPane().add(text);
+	    	
+	    		        
+	    		       Timer cronom=new Timer();
+	    	cronom.schedule(new TimerTask(){
+	    	    
+	    	public void run(){    
+	    		VLogin.tempologin=new VLogin();
+	    		VLogin.tempologin.setVisible(true);
+	    		VLogin.tempologin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    	    dispose();
+	    	}},3000);
 	    }
 
 	   
