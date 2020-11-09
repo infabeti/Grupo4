d0f8b1d0977c4638ed4e0824eb60c8cdc0c938fd
in2daw <in2daw@elorrieta.int>
2020-11-09 19:55:18 +0100

    Creacion de todas las ventanas

---
+++ ProyectoG4/src/main/java/ProyectoG4/VFin.java
@@ -0,0 +1,57 @@
+package ProyectoG4;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Color;
+
+public class VFin extends JFrame {
+
+	private JPanel contentPane;
+	private final JLabel lblNewLabel = new JLabel("La cartelera se ha actualizado");
+	private final JButton btnCancelar = new JButton("Cancelar");
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VFin frame = new VFin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VFin() {
+		initGUI();
+	}
+	private void initGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color (156, 196, 236));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		lblNewLabel.setBounds(125, 68, 153, 14);
+		
+		contentPane.add(lblNewLabel);
+		btnCancelar.setBounds(335, 227, 89, 23);
+		
+		contentPane.add(btnCancelar);
+	}
+
+}
