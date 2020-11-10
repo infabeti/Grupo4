7c9968d206e2599071d45daa2de52571c49def01
unaisantamariape <unai.santamariape@elorrieta-errekamari.com>
2020-11-10 16:19:55 +0100

    imagenes en botones de peliculas e imagen salir

--- ProyectoG4/src/main/java/ProyectoG4/VPeliculas.java
+++ ProyectoG4/src/main/java/ProyectoG4/VPeliculas.java
@@ -15,6 +15,8 @@ import java.awt.event.ContainerEvent;
 import javax.swing.ImageIcon;
 import java.awt.Font;
 import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Cursor;
 
 public class VPeliculas extends JFrame {
 
@@ -23,22 +25,22 @@ public class VPeliculas extends JFrame {
 	private final JLabel lblGeneroSeleccionado = new JLabel("");
 	private final JLabel lblEstassonpelisdisponibles = new JLabel("Estas son las peliculas disponibles:");
 	private final JLabel lbl_genero = new JLabel("Ciencia ficci\u00F3n");
-	private final JButton btn_1_handia = new JButton("Handia");
-	private final JButton btn_1_listaSch = new JButton("La lista de Schindler");
-	private final JButton btn_1_cadenaPerp = new JButton("Cadena Perpetua");
-	private final JButton btn_1_millionDollar = new JButton("Million Dollar Baby");
-	private final JButton btn_4_2001 = new JButton("2001: Odisea en el espacio");
-	private final JButton btn_4_noviaFrank = new JButton("La novia de Frankenstein");
-	private final JButton btn_4_planetaSimios = new JButton("El planeta de los simios");
-	private final JButton btn_4_alien = new JButton("Alien, el octavo pasajero");
-	private final JButton btn_2_scaryMovie = new JButton("Scary Movie");
-	private final JButton btn_2_granLebow = new JButton("El gran Lebowsky");
-	private final JButton btn_2_vidaBrian = new JButton("La vida de Brian");
-	private final JButton btn_2_aterriza = new JButton("Aterriza como puedas");
-	private final JButton btn_3_psicosis = new JButton("Psicosis");
-	private final JButton btn_3_resplandor = new JButton("El resplandor");
-	private final JButton btn_3_dracula = new JButton("Dr\u00E1cula");
-	private final JButton btn_3_cisne = new JButton("Cisne negro");
+	private final JButton btn_1_handia = new JButton("");
+	private final JButton btn_1_listaSch = new JButton("");
+	private final JButton btn_1_cadenaPerp = new JButton("");
+	private final JButton btn_1_millionDollar = new JButton("");
+	private final JButton btn_4_2001 = new JButton("");
+	private final JButton btn_4_noviaFrank = new JButton("");
+	private final JButton btn_4_planetaSimios = new JButton("");
+	private final JButton btn_4_alien = new JButton("");
+	private final JButton btn_2_scaryMovie = new JButton("");
+	private final JButton btn_2_granLebow = new JButton("");
+	private final JButton btn_2_vidaBrian = new JButton("");
+	private final JButton btn_2_aterriza = new JButton("");
+	private final JButton btn_3_psicosis = new JButton("");
+	private final JButton btn_3_resplandor = new JButton("");
+	private final JButton btn_3_dracula = new JButton("");
+	private final JButton btn_3_cisne = new JButton("");
 
 	/**
 	 * Launch the application.
@@ -101,83 +103,131 @@ public class VPeliculas extends JFrame {
 		lbl_genero.setBounds(332, 35, 146, 25);
 		
 		contentPane.add(lbl_genero);
+		btn_1_handia.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_1_handia.setVisible(false);
 		btn_1_handia.setEnabled(false);
+		btn_1_handia.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_1_handia.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\01.Handia.png"));
 		btn_1_handia.setBounds(50, 165, 150, 200);
 		
 		contentPane.add(btn_1_handia);
+		btn_1_listaSch.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_1_listaSch.setVisible(false);
 		btn_1_listaSch.setEnabled(false);
+		btn_1_listaSch.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_1_listaSch.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\01.ListaSch.png"));
 		btn_1_listaSch.setBounds(230, 165, 150, 200);
 		
 		contentPane.add(btn_1_listaSch);
+		btn_1_cadenaPerp.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_1_cadenaPerp.setVisible(false);
 		btn_1_cadenaPerp.setEnabled(false);
+		btn_1_cadenaPerp.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_1_cadenaPerp.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\01.CadenaPerp.png"));
 		btn_1_cadenaPerp.setBounds(410, 165, 150, 200);
 		
 		contentPane.add(btn_1_cadenaPerp);
+		btn_1_millionDollar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_1_millionDollar.setVisible(false);
 		btn_1_millionDollar.setEnabled(false);
+		btn_1_millionDollar.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_1_millionDollar.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\01.MillionDollar.png"));
 		btn_1_millionDollar.setBounds(590, 165, 150, 200);
 		
 		contentPane.add(btn_1_millionDollar);
+		btn_4_2001.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_4_2001.setVisible(false);
 		btn_4_2001.setEnabled(false);
+		btn_4_2001.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_4_2001.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\04.2001.png"));
 		btn_4_2001.setBounds(50, 165, 150, 200);
 		
 		contentPane.add(btn_4_2001);
-		btn_4_noviaFrank.setVisible(false);
+		btn_4_noviaFrank.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_4_noviaFrank.setEnabled(false);
+		btn_4_noviaFrank.setVisible(false);
+		btn_4_noviaFrank.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_4_noviaFrank.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\04.NoviaFrank.png"));
 		btn_4_noviaFrank.setBounds(230, 165, 150, 200);
 		
 		contentPane.add(btn_4_noviaFrank);
+		btn_4_planetaSimios.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_4_planetaSimios.setVisible(false);
 		btn_4_planetaSimios.setEnabled(false);
+		btn_4_planetaSimios.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_4_planetaSimios.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\04.PlanetaSimios.png"));
 		btn_4_planetaSimios.setBounds(410, 165, 150, 200);
 		
 		contentPane.add(btn_4_planetaSimios);
-		btn_4_alien.setVisible(false);
+		btn_4_alien.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_4_alien.setEnabled(false);
+		btn_4_alien.setVisible(false);
+		btn_4_alien.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_4_alien.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\04.Alien.png"));
 		btn_4_alien.setBounds(590, 165, 150, 200);
 		
 		contentPane.add(btn_4_alien);
-		btn_2_scaryMovie.setVisible(false);
+		btn_2_scaryMovie.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_2_scaryMovie.setEnabled(false);
+		btn_2_scaryMovie.setVisible(false);
+		btn_2_scaryMovie.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_2_scaryMovie.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\02.ScaryMovie.png"));
 		btn_2_scaryMovie.setBounds(50, 165, 150, 200);
 		
 		contentPane.add(btn_2_scaryMovie);
-		btn_2_granLebow.setVisible(false);
+		btn_2_granLebow.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_2_granLebow.setEnabled(false);
+		btn_2_granLebow.setVisible(false);
+		btn_2_granLebow.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_2_granLebow.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\02.GranLebow.png"));
 		btn_2_granLebow.setBounds(230, 165, 150, 200);
 		
 		contentPane.add(btn_2_granLebow);
-		btn_2_vidaBrian.setVisible(false);
+		btn_2_vidaBrian.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_2_vidaBrian.setEnabled(false);
+		btn_2_vidaBrian.setVisible(false);
+		btn_2_vidaBrian.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_2_vidaBrian.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\02.VidaBrian.png"));
 		btn_2_vidaBrian.setBounds(410, 165, 150, 200);
 		
 		contentPane.add(btn_2_vidaBrian);
-		btn_2_aterriza.setVisible(false);
+		btn_2_aterriza.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_2_aterriza.setEnabled(false);
+		btn_2_aterriza.setVisible(false);
+		btn_2_aterriza.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_2_aterriza.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\02.Aterriza.png"));
 		btn_2_aterriza.setBounds(590, 165, 150, 200);
 		
 		contentPane.add(btn_2_aterriza);
-		btn_3_psicosis.setVisible(false);
+		btn_3_psicosis.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_3_psicosis.setEnabled(false);
+		btn_3_psicosis.setVisible(false);
+		btn_3_psicosis.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_3_psicosis.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\03.Psicosis.png"));
 		btn_3_psicosis.setBounds(50, 165, 150, 200);
 		
 		contentPane.add(btn_3_psicosis);
-		btn_3_resplandor.setVisible(false);
+		btn_3_resplandor.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_3_resplandor.setEnabled(false);
+		btn_3_resplandor.setVisible(false);
+		btn_3_resplandor.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_3_resplandor.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\03.Resplandor.png"));
 		btn_3_resplandor.setBounds(230, 165, 150, 200);
 		
 		contentPane.add(btn_3_resplandor);
-		btn_3_dracula.setVisible(false);
+		btn_3_dracula.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_3_dracula.setEnabled(false);
+		btn_3_dracula.setVisible(false);
+		btn_3_dracula.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_3_dracula.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\03.Dracula.png"));
 		btn_3_dracula.setBounds(410, 165, 150, 200);
 		
 		contentPane.add(btn_3_dracula);
-		btn_3_cisne.setVisible(false);
+		btn_3_cisne.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btn_3_cisne.setEnabled(false);
+		btn_3_cisne.setVisible(false);
+		btn_3_cisne.setHorizontalTextPosition(SwingConstants.CENTER);
+		btn_3_cisne.setIcon(new ImageIcon("C:\\unaiworkspace\\Grupo4\\ProyectoG4\\imagenes\\Peliculas\\03.CisneNegro.png"));
 		btn_3_cisne.setBounds(590, 165, 150, 200);
 		
 		contentPane.add(btn_3_cisne);
