ba21b021243dcc048be37511fb024bc27901eb6c
Aitor <aitorcanamerosa@elorrieta-errekamari.com>
2020-11-11 16:47:32 +0100

    VFin Completada

--- ProyectoG4/src/main/java/ProyectoG4/VFin.java
+++ ProyectoG4/src/main/java/ProyectoG4/VFin.java
@@ -9,12 +9,20 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
 
 public class VFin extends JFrame {
 
 	private JPanel contentPane;
-	private final JLabel lblNewLabel = new JLabel("La cartelera se ha actualizado");
-	private final JButton btnCancelar = new JButton("Cancelar");
+	private final JLabel lblCarteleraActualizada = new JLabel("La cartelera se ha actualizado");
+	private final JButton btnCancelar = new JButton("Volver");
+	private final JLabel lblNube = new JLabel("");
+	private final JLabel lblNewLabel = new JLabel("");
 
 	/**
 	 * Launch the application.
@@ -36,6 +44,8 @@ public class VFin extends JFrame {
 	 * Create the frame.
 	 */
 	public VFin() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\eclipse-workspace\\Grupo4\\ProyectoG4\\imagenes\\cineCSS.png"));
+		setTitle("Fin");
 		initGUI();
 	}
 	private void initGUI() {
@@ -46,12 +56,31 @@ public class VFin extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		lblNewLabel.setBounds(130, 51, 153, 14);
+		lblCarteleraActualizada.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCarteleraActualizada.setFont(new Font("Freestyle Script", Font.PLAIN, 40));
+		lblCarteleraActualizada.setBounds(35, 97, 364, 38);
 		
-		contentPane.add(lblNewLabel);
+		contentPane.add(lblCarteleraActualizada);
+		btnCancelar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// A la vetana de Bienvenida
+				VBienvenida Bienvenida= new VBienvenida();
+				Bienvenida.setVisible(true);
+				dispose();
+			}
+		});
 		btnCancelar.setBounds(335, 227, 89, 23);
 		
 		contentPane.add(btnCancelar);
+		lblNube.setIcon(new ImageIcon("C:\\eclipse-workspace\\Grupo4\\ProyectoG4\\imagenes\\nube.png"));
+		lblNube.setBounds(0, 11, 414, 239);
+		
+		contentPane.add(lblNube);
+		lblNewLabel.setIcon(new ImageIcon("C:\\eclipse-workspace\\Grupo4\\ProyectoG4\\imagenes\\cineCSS.png"));
+		lblNewLabel.setBounds(0, 0, 89, 84);
+		
+		contentPane.add(lblNewLabel);
 	}
 
 }
